1e7b5e71d13a965057164328013bb1cd101b9444
Mahikiran-Boga <2100030970@kluniversity.in>
2023-12-27 07:50:17 +0530

    Commit Success News API APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import News from './News';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [category, setCategory] = useState('india');
+
+  useEffect(() => {
+    fetchNews();
+  }, [category]);
+
+  const fetchNews = () => {
+    fetch(`https://newsapi.org/v2/everything?q=${category}&from=2023-12-11&apiKey=0e9e67ced62d43c9af654ce043cbcf11`)
+      .then((response) => response.json())
+      .then((news) => {
+        if (news.articles && news.articles.length > 0) {
+          setArticles(news.articles);
+        } else {
+          setArticles([]);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>News App</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            if (event.target.value !== '') {
+              setCategory(event.target.value);
+            } else {
+              setCategory('india');
+            }
+          }}
+          placeholder="Search News"
+        />
       </header>
+      <section className="news-articles">
+        {articles.length !== 0 ? (
+          articles.map((article, index) => {
+            return <News key={index} article={article} />;
+          })
+        ) : (
+          <h1>No News Found for Searched Text</h1>
+        )}
+      </section>
     </div>
   );
 }
